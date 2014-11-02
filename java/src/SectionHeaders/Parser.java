f031c855120b69a97b0687d7d433af460e7d7efb
xsamuhel <samuhel.patrik@gmail.com>
2014-11-02 18:53:35 +0100

    Modify: - XStream -> Stax - odstranene zbytocne subory

--- java/src/SectionHeaders/Parser.java
+++ java/src/SectionHeaders/Parser.java
@@ -6,40 +6,159 @@ package SectionHeaders;
  */
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
-import com.thoughtworks.xstream.XStream;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.events.XMLEvent;
 
 import org.apache.commons.lang3.StringUtils;
 
+
 public class Parser {
 
-	public static void main(String[] args) throws IOException {
-
-		System.out.println(start("data\\sample_input_enwiki-latest-pages-articles1.xml"));
-		System.out.println("koniec");
-
+static int pages = 0;
+	
+	public static void main(String args[]) {
+		
+		String xmlFile = "C:\\VI\\wiki1.xml";
+//		String xmlFile = "data\\sample_input_enwiki-latest-pages-articles1.xml";
+		
+		System.out.println(Start(xmlFile));
+		
+		return;
 	}
 	
-	public static String start(String path){
-		mediawiki data;
-		Root root;
+	public static String Start(String path){
 		
-//		if ((data = dataLoad("data\\enwiki-latest-pages-articles25.xml")) == null) {
-//			return "";
-//		}
+
+		Root root = getItemsFromXml(path);
 		
-		if ((data = dataLoad(path)) == null) {
-			return "";
+		return Output(root, pages);
+	}
+
+//	@SuppressWarnings("unchecked")
+	public static Root getItemsFromXml(String xmlFile) {
+		
+		String startElementName;
+		Root root = null;
+		ArrayList<SectionHeader> sectionList;
+		String title = "";
+		String text = "";
+		
+		try {
+			// create xml reader event with inputstream
+			XMLInputFactory inputFactory = XMLInputFactory.newFactory();
+			InputStream input = new FileInputStream(xmlFile);
+			XMLEventReader inputEventReader = inputFactory
+					.createXMLEventReader(input);
+
+			root = new Root();
+			sectionList = root.getListOfSectionHeaders();
+			
+			while (inputEventReader.hasNext()) {
+				// xml reader event to get event and determine start element and
+				// end element
+				XMLEvent event = inputEventReader.nextEvent();
+
+				if (event.isStartElement()) {
+					startElementName = event.asStartElement().getName()
+							.getLocalPart();
+					
+					if (startElementName.equals("title")) {
+						event = inputEventReader.nextEvent();
+						title = event.asCharacters().getData();
+					}
+
+					if (startElementName.equals("text")) {
+						pages++;
+						event = inputEventReader.nextEvent();
+						text += event.asCharacters().getData();
+						event = inputEventReader.nextEvent();
+						while(!event.isEndElement()) {
+							text += event.asCharacters().getData();
+							event = inputEventReader.nextEvent();
+						}
+						
+						ParseText(sectionList, title, text, root);
+						text = "";
+					}
+
+				}
+
+//				if (event.isEndElement()) {
+//					EndElement endElement = event.asEndElement();
+//					String endElementName = endElement.asEndElement().getName()
+//							.getLocalPart();
+//					if (endElementName.equals("page")) {
+//						//items.add(newItem);
+//					}
+//				}
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		root = parseHeaders(data);
-		
-		return output(root, data);
+		return root;
 	}
 	
-	public static String output(Root root, mediawiki data){
+	private static void ParseText(ArrayList<SectionHeader> sectionList,
+			String title, String text, Root root) {
+		
+		String[] lines = text.split("\n");
+		boolean skip;
+		String header;
+		Article last = null;
+		ArrayList<Article> articleList = null;
+		
+		for (String line : lines) {
+			skip = false;
+			if(line.startsWith("====")){
+				break;
+			}
+			if (line.startsWith("===")) {
+				header = StringUtils.strip(line, "===").trim();
+				last.getSubsecionsList().add(new SubSection(header));
+			} else if (line.startsWith("==")) {
+				header = StringUtils.strip(line, "==").trim();
+				if (sectionList.size() == 0) {
+					sectionList.add(new SectionHeader(header));
+					root.SetinoHeadersCount++;
+					articleList = sectionList.get(sectionList.size() - 1)
+							.getArticlesList();
+					articleList.add(new Article(title));
+					last = articleList.get(articleList.size() - 1);
+					continue;
+				}
+				for (SectionHeader sectionHeader : sectionList) {
+					if (sectionHeader.getname().equals(header)) {
+						sectionHeader.getArticlesList().add(
+								new Article(title));
+						last = sectionHeader.getArticlesList().get(
+								sectionHeader.getArticlesList().size() - 1);
+						skip = true;
+						break;
+					}
+				}
+				if (skip) {
+					continue;
+				} else {
+					sectionList.add(new SectionHeader(header));
+					root.SetinoHeadersCount++;
+					articleList = sectionList.get(sectionList.size() - 1)
+							.getArticlesList();
+					articleList.add(new Article(title));
+					last = articleList.get(articleList.size() - 1);
+					continue;
+				}
+			}
+		}
+		
+		return;
+		
+	}
+
+	public static String Output(Root root, int pages){
 		
 		String maxDFHeader = "";
 		double maxDF = 0;
@@ -47,7 +166,7 @@ public class Parser {
 		
 		outputString += "Output\n\n";
 		for (SectionHeader sectionHeader : root.getListOfSectionHeaders()) {
-			sectionHeader.setDocumentFrequency((double)sectionHeader.getArticlesList().size() / (double)data.getPages().size());
+			sectionHeader.setDocumentFrequency((double)sectionHeader.getArticlesList().size() / (double)pages);
 			outputString += sectionHeader.getname() + ": " + sectionHeader.getDocumentFrequency() + "\n";
 			if(maxDF < sectionHeader.documentFrequency){
 				maxDF = sectionHeader.documentFrequency;
@@ -61,86 +180,5 @@ public class Parser {
 		return outputString;
 	}
 
-	private static Root parseHeaders(mediawiki data) {
-
-		String[] textLines;
-		String header;
-		boolean skip;
-
-		Root root = new Root();
-
-		ArrayList<SectionHeader> headerList = root.getListOfSectionHeaders();
-
-		for (int i = 0; i < data.getPages().size(); i++) {
-			page article = data.getPages().get(i);
-
-			textLines = article.getRevision().getText().split("\n");
-
-			Article last = null;
-			ArrayList<Article> articleList = null;
-
-			for (String line : textLines) {
-				skip = false;
-				if(line.startsWith("====")){
-					break;
-				}
-				if (line.startsWith("===")) {
-					header = StringUtils.strip(line, "===").trim();
-					last.getSubsecionsList().add(new SubSection(header));
-				} else if (line.startsWith("==")) {
-					header = StringUtils.strip(line, "==").trim();
-					if (headerList.size() == 0) {
-						headerList.add(new SectionHeader(header));
-						root.SetinoHeadersCount++;
-						articleList = headerList.get(headerList.size() - 1)
-								.getArticlesList();
-						articleList.add(new Article(article.getTitle()));
-						last = articleList.get(articleList.size() - 1);
-						continue;
-					}
-					for (SectionHeader sectionHeader : headerList) {
-						if (sectionHeader.getname().equals(header)) {
-							sectionHeader.getArticlesList().add(
-									new Article(article.getTitle()));
-							last = sectionHeader.getArticlesList().get(
-									sectionHeader.getArticlesList().size() - 1);
-							skip = true;
-							break;
-						}
-					}
-					if (skip) {
-						continue;
-					} else {
-						headerList.add(new SectionHeader(header));
-						root.SetinoHeadersCount++;
-						articleList = headerList.get(headerList.size() - 1)
-								.getArticlesList();
-						articleList.add(new Article(article.getTitle()));
-						last = articleList.get(articleList.size() - 1);
-						continue;
-					}
-				}
-			}
-		}
-
-		return root;
-	}
-
-	private static mediawiki dataLoad(String inputPath) {
-
-		try {
-			InputStream fr = new FileInputStream(inputPath);
-
-			// inicializacia parseru a anotacii jednotlivych xml tagov
-			XStream xstream = new XStream();
-			xstream.processAnnotations(mediawiki.class);
-			xstream.processAnnotations(page.class);
-			xstream.processAnnotations(revision.class);
-			return (mediawiki) xstream.fromXML(fr);
-		} catch (Exception ex) {
-			System.out.println("dataLoad error: " + ex.getMessage());
-			return null;
-		}
-	}
 
 }
